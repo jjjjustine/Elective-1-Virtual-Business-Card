b80992d558980f6625783a825c98853a6a3d4f65
jjjjustine <catingan.janejustine@gmail.com>
2024-11-21 10:54:06 +0800

    added changes

--- script.js
+++ script.js
@@ -2,13 +2,7 @@ const cards = document.querySelectorAll('.card');
 
 cards.forEach(card => {
     card.addEventListener('click', () => {
+        // Toggle zoom effect on click
         card.classList.toggle('zoom');
     });
 });
-
-// Optional: Add a zoom effect on click
-document.styleSheets[0].insertRule(`
-    .card.zoom .card-inner {
-        transform: scale(1.2);
-    }
-`, document.styleSheets[0].cssRules.length);
